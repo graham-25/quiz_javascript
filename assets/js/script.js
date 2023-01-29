07ee7c8e8b3e3345b6e4b118637c7c4f3b09b8e5
graham-25 <grbills29@googlemail.com>
2023-01-29 09:24:15 +0000

    Got a working Array of Objects listing 6 individual elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const startButton = document.getElementById('btn-start')
 
-startButton.addEventListener('click' this.startGame)
+//startButton.addEventListener('click' this.startGame)
 
 function startGame() {
     let gameCount = 0;
@@ -12,23 +12,68 @@ function displayQuestion() {
     
 }
 
+function checkAnswer() {
 
-const questions [
+}
+
+
+/* This is where the Array of questions will be, 
+I've put objects inside an Array of questions and 
+I'm trying to write code to interact with code. */  
+
+const questions = [
+  
     {
         question: "What is the capital of United Kingdom?", 
-        answer: 
-        {text: "Manchester", correct: false },
-        {text: "London", correct: true },
-        {text: "Birmingham", correct: false },
-        {text: "Newcastle", correct: false }
-    }
+        answer1: "Manchester",
+        answer2: "London",
+        answer3: "Birmingham",
+        answer4: "Newcastle",
+        correct: 2
+    },
 
     {
         question: "What colour is grass?",
-        answer:
-        {text: "Blue", correct: false },
-        {text: "yellow", correct: false },
-        {text: "Green", correct: true },
-        {text: "Red", correct: false }
+        answer1: "Blue",
+        answer2: "yellow",
+        answer3: "Green",
+        answer4: "Red",
+        correct: 3
+    },
+
+    {
+        question: "How many Spice Girls where their back in the 90's",
+        answer1: 3,
+        answer2: 7,
+        answer3: 5,
+        answer4: 9,
+        correct: 3
+    },
+
+    {
+        question: "What does WWW stand for in computing?",
+        answer1: "Washing, Wiping, Wimpering",
+        answer2: "World Wide Web",
+        answer3: "Words With Wepons",
+        answer4: "What Windy Women",
+        correct: 2
+    },
+
+    {
+        question: "What is the capital of america?",
+        answer1: "Ohio",
+        answer2: "Washington DC",
+        answer3: "Alabama",
+        answer4: "pencilvainer",
+        correct: 2
+    },
+
+    {
+        question: "What did Donald Trump wanted to use to keep out the maxicans?",
+        answer1: "G.I Jane",
+        answer2: "Arnold Schortinigger",
+        answer3: "Wall",
+        answer4: "M16 Assolt Rifel",
+        correct: 3
     }
 ]
