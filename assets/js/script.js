771f8f858a0b1a254314d3c4a15dec43d184b35a
graham-25 <grbills29@googlemail.com>
2023-01-31 08:02:40 +0000

    got system to show questions and answers, tidied up my arrays and keeping score.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,60 @@
-/*
-document.addEventListener("DOMContentLoaded", function() (
-   // let buttons = document.getElementsByTagName('button');
 
-    for(let i = 0; i < buttons.length; i++) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === 'start') {
-                alert("You clicked GOOD");
-            } else {
-                let gameType = this.getAttribute("data-type");
-                alert('You clicked' ${gameType}); 
-            }
-        })
-    }
-)) 
-*/
+const startButton = document.getElementById('btn-start');
+const endButton = document.getElementById('btn-end');
+const questionElement = document.getElementById('displayQuestion');
+const answerButtons = document.getElementsByClassName('answerBTNs');
 
-const startButton = document.getElementById('btn-start')
-const endButton = document.getElementById('btn-end')
-const questionElement = document.getElementById('displayQuestion')
+let currentQuestion;
+let scores;
 
-startButton.addEventListener('click', this.startGame)
-endButton.addEventListener('click', this.endGame)
+startButton.addEventListener('click', this.startGame);
+endButton.addEventListener('click', this.endGame);
 
 
 /*
-This is the first function that will be called, it initalises, clears the 
-game board and starts the game. 
+This is the first function that will be called, it initalises score and current question to 0, 
+calls the next function display question. 
 */
 function startGame() {
-    console.log('Game started');
-    alert('Game Started');
-    let gameCount = 0;
+    score = 0;
+    currentQuestion = 0;
     displayQuestion()
-    console.log('gameCount');
-
 }
 
 /*
-This is the function that will display our question inthe HTML page,
+This is the function that will display our question in the HTML page,
+first it checks to see the length of the questions array is not more than
+the variable current question. It then displays the current question and the answers.
 */
 function displayQuestion() {
-    document.getElementById('displayQuestion').textContent = "Hello World"
-        //questionElement.innerText = questions.question
-}
+    console.log("Current question is " + currentQuestion);
+    if ((currentQuestion + 1) > questions.length) {
+        endGame();
+    }
+    questionElement.innerHTML = questions[currentQuestion].question;
+    for (let i = 1; i < 5; i++) {
+        document.getElementById('btn' + i).innerHTML = questions[currentQuestion].answers[i - 1];
+        document.getElementById('btn' + i).dataset.type = questions[currentQuestion].answers[i - 1];
+    } 
+};
+
     
+/*
 
-function checkAnswer() {
-
+*/
+function checkAnswer(e) {
+    console.log(e.dataset.type);
+    if (e.dataset.type == questions[currentQuestion].correct) {
+        score++;
+    }
+    console.log("Score is " + score);
+    currentQuestion++;
+    displayQuestion();
 }
 
 function endGame() {
     alert('Game Over');
+
 
 }
 
@@ -62,56 +66,37 @@ I'm trying to write code to interact with code. */
 const questions = [
   
     {
-        question: "What is the capital of United Kingdom?", 
-        answer1: "Manchester",
-        answer2: "London",
-        answer3: "Birmingham",
-        answer4: "Newcastle",
-        correct: 2
+        question: "What is the capital of the United Kingdom?", 
+        answers: ["Manchester", "London", "Birmingham", "Newcastle"],
+        correct: "London"
     },
-
     {
         question: "What colour is grass?",
-        answer1: "Blue",
-        answer2: "yellow",
-        answer3: "Green",
-        answer4: "Red",
-        correct: 3
+        answers: ["Blue", "Yellow", "Green", "Red"],
+        correct: "Green"
     },
 
     {
         question: "How many Spice Girls where their back in the 90's",
-        answer1: 3,
-        answer2: 7,
-        answer3: 5,
-        answer4: 9,
-        correct: 3
+        answers: [3, 7, 5, 9],
+        correct: 5
     },
 
     {
         question: "What does WWW stand for in computing?",
-        answer1: "Washing, Wiping, Wimpering",
-        answer2: "World Wide Web",
-        answer3: "Words With Wepons",
-        answer4: "What Windy Women",
-        correct: 2
+        answers: ["Washing, Wiping, Wimpering", "World Wide Web", "Words With Wepons", "What Windy Women"],
+        correct: "World Wide Web"
     },
 
     {
-        question: "What is the capital of america?",
-        answer1: "Ohio",
-        answer2: "Washington DC",
-        answer3: "Alabama",
-        answer4: "pencilvainer",
-        correct: 2
+        question: "What is the capital of the United States of America?",
+        answers: ["Phoenix", "Washington DC", "Montgomery", "Philadelphia"],
+        correct: "Washington DC"
     },
 
     {
-        question: "What did Donald Trump wanted to use to keep out the maxicans?",
-        answer1: "G.I Jane",
-        answer2: "Arnold Schortinigger",
-        answer3: "Wall",
-        answer4: "M16 Assolt Rifel",
-        correct: 3
+        question: "What did Donald Trump wanted to use to keep out the Mexicans?",
+        answers: ["G.I Jane", "Arnold Schortinigger", "Wall", "M16 Assault Rifle"],
+        correct: "Wall"
     }
 ]
